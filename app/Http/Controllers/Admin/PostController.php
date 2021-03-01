abcd9cdb3f08894d713d70a1d9bb0b171f671b67
williamtruppi <williamt@hotmail.it>
2021-03-01 16:52:02 +0100

    implementata funzione getRouteKeyName con relativo post visualizzato

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -48,7 +48,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
