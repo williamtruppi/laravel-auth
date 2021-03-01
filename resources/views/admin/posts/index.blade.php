367eff7db22cd8037c87b718c0a77eedf078835f
williamtruppi <williamt@hotmail.it>
2021-03-01 16:37:17 +0100

    post correttamente visualizzati nel posts.index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,7 +3,30 @@
 @section('content')
     <h1>All post for the Admin</h1>
 
-    @foreach (posts as $post)
-        
-    @endforeach
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Slug</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+            <tr>
+                <td scoper="row">{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->slug }}</td>
+                <td>
+                    <a href="{{route('admin.posts.show', ['post' => $post->slug])}}" class="btn btn-primary"><i class="fas fa-eye fa-xs fa-fw"></i></a>
+                    <a href="#" class="btn btn-warning"><i class="fas fa-pencil-ruler fa-xs fa-fw"></i></a>
+                    <a href="#" class="btn btn-danger"><i class="fas fa-trash fa-xs fa-fw"></i></a>
+                </td>
+            </tr>
+                
+            @endforeach
+        </tbody>
+    </table>
+    
 @endsection
