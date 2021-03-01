44663a95b97ed58f992dfa6a0898f81a2c2afb69
williamtruppi <williamt@hotmail.it>
2021-03-01 16:17:31 +0100

    disattivata la possibilità di registrarsi

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', 'PageController@index')->name('homepage');
 Route::get('about', 'PageController@about')->name('about');
 Route::get('contacts', 'PageController@contacts')->name('contacts');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', 'HomeController@index')->name('index');
